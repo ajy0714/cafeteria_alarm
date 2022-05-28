60498c9e4512b996c4a66767825ed33456637f74
ajy <swh01007@gmail.com>
2022-05-28 13:12:43 +0900

    object_type: feed 연습

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # 삼환식당 구내식당 알림봇
+
+
+텍스트 타입으로 할 땐 object_type을 text로 놓고 해야함
+    "object_type": "text",
+    "text": menu['MENU_DESC'],
+    "link": {
+        "web_url": "https://www.google.co.kr/search?q=drone&source=lnms&tbm=nws",
+        "mobile_web_url": "https://www.google.co.kr/search?q=drone&source=lnms&tbm=nws"
+    }wo
--- kakao2.py
+++ kakao2.py
@@ -32,25 +32,88 @@ with open("kakao_code.json", "r") as fp:
 
 # print(friends_list)
 
-
 url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
 headers = {
-    "Authorization": "Bearer " + "hgsf8UksJj4SDyvXyUSPf7efu5usaxtj3d1q6oOyCinJXgAAAYEAxV_m",
+    "Authorization": "Bearer " + tokens['access_token'],
     "Content-Type": "application/x-www-form-urlencoded"
 }
-data = {
-    "template_object": json.dumps({
-        "object_type": "text",
-        "text": menu['MENU_DESC'],
-        "link": {
-            "web_url": "https://www.google.co.kr/search?q=drone&source=lnms&tbm=nws",
-            "mobile_web_url": "https://www.google.co.kr/search?q=drone&source=lnms&tbm=nws"
-        }
-    })
-}
 
-response = requests.post(url, headers=headers, data=data)
+params = {}
+# params['receiver_uuids'] = '["abcdefg0001"]'
+params['template_object'] = json.dumps({
+    "object_type": "feed",
+        "content": {
+            "title": "오늘의 디저트",
+            "description": "아메리카노, 빵, 케익",
+            "image_url": "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+            "image_width": 640,
+            "image_height": 640,
+            "link": {
+                "web_url": "http://www.daum.net",
+                "mobile_web_url": "http://m.daum.net",
+                "android_execution_params": "contentId=100",
+                "ios_execution_params": "contentId=100"
+            }
+        },
+        "item_content" : {
+            "profile_text" :"Kakao",
+            "profile_image_url" :"https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+            "title_image_url" : "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+            "title_image_text" :"Cheese cake",
+            "title_image_category" : "Cake",
+            "items" : [
+                {
+                    "item" :"Cake1",
+                    "item_op" : "1000원"
+                },
+                {
+                    "item" :"Cake2",
+                    "item_op" : "2000원"
+                },
+                {
+                    "item" :"Cake3",
+                    "item_op" : "3000원"
+                },
+                {
+                    "item" :"Cake4",
+                    "item_op" : "4000원"
+                },
+                {
+                    "item" :"Cake5",
+                    "item_op" : "5000원"
+                }
+            ],
+            "sum" :"Total",
+            "sum_op" : "15000원"
+        },
+        "social": {
+            "like_count": 100,
+            "comment_count": 200,
+            "shared_count": 300,
+            "view_count": 400,
+            "subscriber_count": 500
+        },
+        "buttons": [
+            {
+                "title": "웹으로 이동",
+                "link": {
+                    "web_url": "http://www.daum.net",
+                    "mobile_web_url": "http://m.daum.net"
+                }
+            },
+            {
+                "title": "앱으로 이동",
+                "link": {
+                    "android_execution_params": "contentId=100",
+                    "ios_execution_params": "contentId=100"
+                }
+            }
+        ]
+})
+
+response = requests.post(url, headers=headers, params=params)
+
 if response.json().get('result_code') == 0:
     print('메시지를 성공적으로 보냈습니다.')
 else:
-    print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
+    print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.content))
